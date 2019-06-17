8234174ccfa4aa96253005fd9f5e4fbfbf1c22e7
Thais Oliveira <borgesoliveira.thais@gmail.com>
2019-06-17 14:58:27 -0300

    testes no serviço de auth-guard e readme atualizado

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.0.
 
+## Rodando a aplicação
+
+- npm install
+- npm run server (para rodar server json)
+- npm start (para rodar a aplicação)
+- Usuário: callum | Senha: arcanum
+
+## Importante 
+
+- Este projeto foi feito com carinho e em um período curto de tempo. O nome Arcanum vem de uma série animada do Netflix e é referente a conexão dos seres daquele universo com a magia. Callum, o nome de usuário também é um dos personagens e ele é um humano apaixonado por magia, porém naquele universo os humanos não tem conexão com nenhuma das magias primárias, basicamente não tem arcanum. Só que ele acaba aprendendo a se conectar com isso e bem, foi por isso que usei ele como parte do contexto. 
+- Infelizmente minha experiência prática com teste não é tão grande, porém me esforcei para pelo menos deixar o serviço de auth-guard com alguns testes. Se tiver mais tempo e espaço para aprender com certeza expandiria meus conhecimentos sobre testes e praticaria mais.
+
+## Libs utilizadas
+
+- material;
+- json-server
+- ngx-cookie-service
+- primeng
+- ngx-toastr
+- normalize.css
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/_services/auth-guard.service.spec.ts
+++ src/app/_services/auth-guard.service.spec.ts
@@ -1,12 +1,49 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthGuard } from './auth-guard.service';
 
-import { AuthGuardService } from './auth-guard.service';
+fdescribe('AuthGuard', () => {
+	let service: AuthGuard;
+	let cookieService: CookieService;
+	let routerSpy;
 
-describe('AuthGuardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+	beforeEach(() => {
 
-  it('should be created', () => {
-    const service: AuthGuardService = TestBed.get(AuthGuardService);
-    expect(service).toBeTruthy();
-  });
+		TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+			],
+			providers: [
+				CookieService
+			]
+		});
+
+		routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+		service = TestBed.get(AuthGuard);
+		cookieService = TestBed.get(CookieService);
+		cookieService.deleteAll();
+
+	});
+
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
+
+	it('should return true when user is authenticated', () => {
+		cookieService.set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSm9obkRvZSIsInBhc3N3b3JkIjoiMTUxNjIzOTAyMiJ9.nldKjarqIty3eFv-pc7wT5dVMa-mJbWBbhGIWH-pXwc');
+		expect(service.canActivate()).toBeTruthy();
+	});
+
+	it('should return false when is not authenticated', () => {
+		expect(service.canActivate()).toBeFalsy();
+	});
+
+	it('should redirect page to login when user is not authenticated', () => {
+		const authGuardService = new AuthGuard(routerSpy, cookieService);
+		authGuardService.canActivate();
+		expect(routerSpy.navigate).toHaveBeenCalledWith(['/login']);
+	});
+
 });
+
